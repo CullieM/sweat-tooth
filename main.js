faf3dffecc802efaf2fe719de28b22cbb3b8f91b
Cullie McElduff <contact@cullie.dev>
2025-03-29 13:37:32 +1100

    Don't all play at once

--- main.js
+++ main.js
@@ -1,32 +1,51 @@
 import './style.css'
-let appleMusic = false;
 let spotifySongIds = [
-  '4FVEwchgZknBl3qjE2h4Y6?si=7b33a982c7de40d2',
-  '0jV5qFgT7QEJJPbPCuZtWP?si=c39a77e082124ce3',
-  '7nSoBBeqOvCSUeBpqYo2EU?si=AcHbMANJT7iQ5I-UEQ2LpQ',
-];
+	'4FVEwchgZknBl3qjE2h4Y6?si=7b33a982c7de40d2',
+	'0jV5qFgT7QEJJPbPCuZtWP?si=c39a77e082124ce3',
+	'7nSoBBeqOvCSUeBpqYo2EU?si=AcHbMANJT7iQ5I-UEQ2LpQ',
+];	
+let songEmbedsHTML = '';
+let playingEmbedIds = [];
+let latestPlayedEmbed = 0;
+let counter = 1;
 
-let innerHTML = '<div>';
-
-spotifySongIds.forEach( songId => {
-  innerHTML += 
-  `<div class="embed-spotify-song">
-  <iframe src="https://open.spotify.com/embed/track/${songId}"  
-            frameborder="0" 
-            allowtransparency="true" 
-            allow="encrypted-media">
-  </iframe>
-  </div>
-  <div id="padding"></div>
-  `
-});
-
-if (appleMusic) {
-  innerHTML += `
-    <div style="left: 0; width: 100%; height: 450px; position: relative;">
-      <iframe src="https://embed.music.apple.com/au/album/badboygoodgirl-single/1765465150" style="top: 0; left: 0; width: 100%; height: 100%; position: absolute; border: 0;" allowfullscreen></iframe>
-    </div>`;
+for (let i = 1; i <= spotifySongIds.length; i++) {
+	songEmbedsHTML +=
+		`<div class="embed-spotify-song">
+			<iframe class="spotify-iframe" id="embed${i}"
+						frameborder="0" 
+						allowtransparency="true" 
+						allow="encrypted-media">
+			</iframe>
+		</div>
+		<div id="padding"></div>`;
 }
-innerHTML += `</div>`;
+document.querySelector('#app').innerHTML = `<div>${songEmbedsHTML}</div>`;
 
-document.querySelector('#app').innerHTML = innerHTML;
+window.onSpotifyIframeApiReady = (IFrameAPI) => {
+	spotifySongIds.forEach(songId => {
+		let embedDomElement = document.getElementById('embed' + counter);
+		let embedOptions = {
+			uri: 'spotify:track:' + songId,
+			width: '300',
+			height: '80',
+		};
+		let callbackFunc = (EmbedController) => {
+			let embedId = counter;
+			EmbedController.addListener('playback_update', e => {
+				// Is playing but hasn't been recorded yet, mark as latest playing and record as playing
+				if (!e.data.isPaused && !playingEmbedIds.includes(embedId) && latestPlayedEmbed != embedId) {
+					latestPlayedEmbed = embedId;
+					playingEmbedIds.push(embedId);
+				}
+				// Is playing but isn't the most recent playing, pause it and remove from playing list
+				if (latestPlayedEmbed != 0 && playingEmbedIds.includes(embedId) && latestPlayedEmbed != embedId) { 
+					EmbedController.pause();
+					playingEmbedIds.splice(playingEmbedIds.indexOf(embedId), 1);
+				}
+			});
+		};
+		IFrameAPI.createController(embedDomElement, embedOptions, callbackFunc);
+		counter++;
+	});
+};
